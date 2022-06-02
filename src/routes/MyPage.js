4c7a96cf7c640758ef4ee772facdbdde985159c9
Suhyeon Lee <suhyeonlee.dev@gmail.com>
2022-06-02 14:23:10 +0800

    design: my page style

--- src/routes/MyPage.js
+++ src/routes/MyPage.js
@@ -1,9 +1,10 @@
-import { BiUserCircle } from 'react-icons/bi';
+import { FaUserCircle } from 'react-icons/fa';
 import Post from 'components/Post';
 import React, { useEffect, useState } from 'react';
 import GoalCard from 'components/GoalCard';
 import { collection, getDocs, query, where } from 'firebase/firestore';
 import { dbService } from 'fbase';
+import styles from 'assets/styles/MyPage.module.css';
 
 const MyPage = ({ userObj }) => {
   const [content, setContent] = useState('goals');
@@ -48,29 +49,50 @@ const MyPage = ({ userObj }) => {
   };
 
   return (
-    <div>
+    <div className="main-page">
       <h2>My page</h2>
-      <div>
-        <BiUserCircle />
-        <span>{userObj.displayName}</span>
+      <div className={styles.profile}>
+        <FaUserCircle className={styles['profile-icon']} />
+        <span className={styles['profile-username']}>
+          {userObj.displayName}
+        </span>
       </div>
-      <div>
-        <button onClick={onMenuClick} value="goals">
-          Goals &amp; Rewards
-        </button>
-        <button onClick={onMenuClick} value="feed">
-          Challenge Feed
-        </button>
+      <div className={styles.nav}>
+        <div className={styles['nav-menu-container']}>
+          <button
+            className={`${styles['nav-menu']} ${
+              content === 'goals' && styles.active
+            }`}
+            onClick={onMenuClick}
+            value="goals"
+          >
+            Goals &amp; Rewards
+          </button>
+          <button
+            className={`${styles['nav-menu']} ${
+              content === 'feed' && styles.active
+            }`}
+            onClick={onMenuClick}
+            value="feed"
+          >
+            Challenge Feed
+          </button>
+        </div>
+        <div
+          className={`${styles['active-bar']} ${
+            content === 'goals' && styles.goals
+          } ${content === 'feed' && styles.feed}`}
+        ></div>
       </div>
       {content === 'goals' && (
-        <div>
+        <div className={styles.goals}>
           {goals.map((goal) => (
             <GoalCard key={goal.id} goalObj={goal} isOwner={true} />
           ))}
         </div>
       )}
       {content === 'feed' && (
-        <div>
+        <div className={styles.feed}>
           {userObj.userPosts.map((post, index) => (
             <Post
               key={index}
