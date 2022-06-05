e69eab6f5bd6f5545e0f1e78afc1dc3da034c787
Suhyeon Lee <suhyeonlee.dev@gmail.com>
2022-06-05 22:00:48 +0800

    fix: challenge card category + category image border radius

--- src/components/ChallengeCard.js
+++ src/components/ChallengeCard.js
@@ -9,26 +9,38 @@ const ChallengeCard = ({ challengeObj, onCardClick }) => {
     onCardClick(challengeObj);
   };
 
+  const getFrequency = () => {
+    switch (challengeObj.frequency) {
+      case 'everyday':
+        return 'every day';
+      case 'five':
+        return '5 times a week';
+      case 'three':
+        return '3 times a week';
+      case 'twice':
+        return 'twice a week';
+      case 'once':
+        return 'once a week';
+      default:
+    }
+  };
+
   return (
     <div className={styles.card} onClick={onCardOpen}>
       <div className={styles['card-img']}>
         <CategoryImg challengeObj={challengeObj} usage={'card'} />
       </div>
       <div className={styles['card-content']}>
-        <div className={styles['card-info']}>
-          <div className={styles['card-categories']}>
-            <button className={styles['card-category']}>
-              {challengeObj.category}
-            </button>
-            <button className={styles['card-category']}>
-              {challengeObj.frequency}
-            </button>
-            <button className={styles['card-category']}>
-              {challengeObj.days}
-            </button>
-          </div>
-          <span className={styles['card-title']}>{challengeObj.title}</span>
+        <div className={styles['card-categories']}>
+          <button className={styles['card-category']}>
+            {challengeObj.category}
+          </button>
+          <button className={styles['card-category']}>
+            {challengeObj.days} days
+          </button>
+          <button className={styles['card-category']}>{getFrequency()}</button>
         </div>
+        <span className={styles['card-title']}>{challengeObj.title}</span>
         <button className={styles['card-right-icon']}>
           <HiOutlineChevronRight />
         </button>
